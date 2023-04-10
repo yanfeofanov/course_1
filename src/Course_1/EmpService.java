3f95c28ff53499d60e8a0a3b8b342020412707bc
kaVII-x <ka52jiguli@yandex.ru>
2023-04-10 03:45:28 +0200

    Готово

--- src/Course_1/EmpService.java
+++ src/Course_1/EmpService.java
@@ -1,90 +1,91 @@
 package Course_1;
 
+import java.util.*;
+
 public class EmpService {
 
-    private final Employee[] employees;
-    private int size;
+    private final Map<String, Employee> employeeMap = new HashMap<>();
 
 
 
 
-    public EmpService(){                                    // Приводим все к массиву данных
-        this.employees = new Employee[10];
+    public void addEmployee(Employee employee) {  // Метод добавления сотрудника в массив
+        employeeMap.put(employee.getFullName(), employee);
     }
 
-    public void addEmployee(String employeeFullName, int employeeDepartment, float employeeSalary){  // Метод добавления сотрудника в массив
-        if (employeeFullName.isEmpty()){
-            System.out.println("ФИО не может быть пустым полем ");
-
-        }
-        else if(size >= employees.length ){
-            System.out.println("Нельзя добавить сотрудника, закончилось место!");
-        }
-        else {
-            Employee newEmployee = new Employee(employeeFullName,employeeDepartment,employeeSalary);
-            employees[size++] = newEmployee;
-
-        }
+    public void removeEmployee(String name) {
+        employeeMap.remove(name);
     }
-    public float totalAmount() {                                // Метод общей суммы зарплаты
+
+    public void totalAmount() {
         int sum = 0;
-        for (int i = 0; i < size; i++) {
-          sum += employees[i].getSalary();
+        for (Employee value : employeeMap.values()) {
+            sum = (int) (value.getSalary() + sum);
         }
-        return sum;
+        System.out.println(sum);
     }
 
-    public String maxSalary(){                              // Поиск сотрудника с высоким заработком
-        float max = 0;
-        String employeeMaxSalary = "";
-        for (int i = 0; i < size; i++) {
-                if(employees[i].getSalary()>max){
-                    max=employees[i].getSalary();
-                    employeeMaxSalary=employees[i].getFullName();
-
-                }
-        }
-        return employeeMaxSalary;
+    public void averageAmount(){
+     
     }
-    public String minSalary(){                                        // Поиск сотрудника с низким заработком
-        float min = employees[0].getSalary();
-        String employeeMaxSalary = "";
-        for (int i = 0; i < size; i++) {
-            if(employees[i].getSalary()<min){
-                min=employees[i].getSalary();
-                employeeMaxSalary=employees[i].getFullName();
-
+    public void maxSalary() {
+        int max = 0;
+        String name = " ";
+        for (Employee value : employeeMap.values()) {
+            if (value.getSalary() > max) {
+                max = (int) value.getSalary();
+                name = value.getFullName();
             }
         }
-        return employeeMaxSalary;
+        System.out.println("Самая высокая зп у " + name + " составляет : " + max);
     }
-    public void printOnlyEmployees() {                                   // Вывод только ФИО сотрудников
-        for (int i = 0; i < size; i++) {
-            System.out.println(employees[i].getFullName());
 
+    public void minSalary() {
+        int min = employeeMap.values().hashCode();
+        String name = " ";
+        for (Employee value : employeeMap.values()) {
+            if (value.getSalary() < min) {
+                min = (int) value.getSalary();
+                name = value.getFullName();
+            }
+        }
+        System.out.println("Самая низкая зп у " + name + " составляет : " + min);
+    }
+
+    public void searchOnlyEmployees(String name){
+        employeeMap.get(name);
+    }
+
+
+
+
+//    public void printOnlyEmployees() {                                   // Вывод только ФИО сотрудников
+//        for (int i = 0; i < size; i++) {
+//            System.out.println(employees[i].getFullName());
+//
+//        }
+//    }
+//    public float averageAmount(){                                   // Метод средней суммы зп
+//        return totalAmount()/size;
+//    }
+//
+    public void printAllEmployees() {                                  // Вывод всех параметров массива ID ФИО Отдел зарплата
+        for (Employee value : employeeMap.values()) {
+            System.out.println(value);
         }
     }
-    public float averageAmount(){                                   // Метод средней суммы зп
-        return totalAmount()/size;
-    }
 
-    public void printAllEmployees(){                                  // Вывод всех параметров массива ID ФИО Отдел зарплата
-        for (int i = 0; i < size; i++) {
-            Employee employee = employees[i];
-            System.out.println(employee);
 
-        }
-
-        System.out.println();
-        System.out.println("Всего в компании работает : "+size+" сотрудника");
-        System.out.println();
-        System.out.printf("Общая сумма трат на зарплату сотрудников составила: %.2f рублей,за один месяц",totalAmount());
-        System.out.println();
-        System.out.println("Сотрудник "+minSalary()+" имеет самую низкую зарплату в компании");
-        System.out.println("Сотрудник "+maxSalary()+" имеет самую высокую зарплату в компании");
-        System.out.printf("Среднее значение зарплат за месяц составило: %.2f рублей",averageAmount());
-        System.out.println();
-        System.out.println();
-    }
+//        System.out.println();
+//        System.out.println("Всего в компании работает : "+size+" сотрудника");
+//        System.out.println();
+//        System.out.printf("Общая сумма трат на зарплату сотрудников составила: %.2f рублей,за один месяц",totalAmount());
+//        System.out.println();
+//        System.out.println("Сотрудник "+minSalary()+" имеет самую низкую зарплату в компании");
+//        System.out.println("Сотрудник "+maxSalary()+" имеет самую высокую зарплату в компании");
+//        System.out.printf("Среднее значение зарплат за месяц составило: %.2f рублей",averageAmount());
+//        System.out.println();
+//        System.out.println();
+//    }
 
 }
--- src/Course_1/PrintInfo.java
+++ src/Course_1/PrintInfo.java
@@ -1,42 +1,23 @@
 package Course_1;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrintInfo {
     public static void main(String[] args) {
 
-        Employee employee1 = new Employee();
-        employee1.setDepartment(2);
-        employee1.setSalary(180653);
-        Employee employee2 = new Employee();
-        employee2.setDepartment(1);
-        employee2.setSalary(150680);
-        Employee employee3 = new Employee();
-        employee3.setDepartment(5);
-        employee3.setSalary(30808);
-        Employee employee4 = new Employee();
-        employee4.setDepartment(3);
-        employee4.setSalary(49092);
-        Employee employee5 = new Employee();
-        employee5.setDepartment(4);
-        employee5.setSalary(12399);
+        EmpService empService = new EmpService();
+        empService.addEmployee(new Employee("Феофанов Ян Олегович", 1, 233));
+        empService.addEmployee(new Employee("Самаров Алексей Вячеславович", 2, 34));
+        empService.addEmployee(new Employee("Еремеев Алексей Сергеевич", 3, 42));
 
-
-
-        EmpService employees = new EmpService();
-        employees.addEmployee("Феофанов Ян Олегович        ", employee1.getDepartment(), employee1.getSalary());
-        employees.addEmployee("Самаров Алексей Вячеславович",employee2.getDepartment(),employee2.getSalary());
-        employees.addEmployee("Кабанов Елисей Донатович    ",employee3.getDepartment(),employee3.getSalary());
-        employees.addEmployee("Давыдов Адам Альвианович    ",employee4.getDepartment(),employee4.getSalary());
-        employees.addEmployee("Шарипов Роберт Степанович   ",employee5.getDepartment(),employee5.getSalary());
-        employees.addEmployee("Беляков Артем Юлианович     ",1,35060);
-        employees.addEmployee("Голубев Кирилл Гордеевич    ",2,15615);
-        employees.addEmployee("Белов Архип Филиппович      ",3,65488);
-        employees.addEmployee("Горбунов Клим Христосович   ",4,70568);
-        employees.addEmployee("Филатов Осип Эльдарович     ",5,78745);
-
-
-        employees.printAllEmployees();
-        employees.printOnlyEmployees();
-
-
+        empService.totalAmount();
+        empService.removeEmployee("Самаров Алексей Вячеславович");
+        empService.printAllEmployees();
+        empService.maxSalary();
+        empService.minSalary();
+        empService.searchOnlyEmployees("Феофанов Ян Олегович");
     }
+
+
 }
